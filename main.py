163e2c5e88a194ba0a9f3b1758caaa6a96254fad
Burncoat <htvick@gmail.com>
2025-02-03 15:48:51 -0500

    Moveable traingle

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #game loop
@@ -15,9 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
+        for item in drawable:
+            item.draw(screen)
         dt = clock.tick(60) / 1000
+        updatable.update(dt)
+        pygame.display.flip()
         
 print("Starting asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
